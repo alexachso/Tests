03ba4270921d8c297b93e7de04bc7655873692dd
Alexandru Neamtu <alexandru.stefan.neamtu@emd-tech.net>
2023-11-30 13:50:54 -0500

    Tried another method.

---
+++ EnumCheckboxes/Converters/EnumDescription.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace EnumCheckboxes.Converters;
+
+internal class EnumDescription : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Enum myEnum && myEnum.GetType().GetCustomAttribute(typeof(FlagsAttribute)) != null)
+        {
+            FieldInfo? fi = myEnum.GetType().GetField(myEnum.ToString());
+            DescriptionAttribute[]? attributes = (DescriptionAttribute[]?)fi?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+        }
+
+        throw new ArgumentException("The object passed to the converter is not of type Enum or it does not have the attribute Flags!");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ EnumCheckboxes/Converters/FlagsEnumValueToBoolMultiConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace EnumCheckboxes.Converters;
+
+internal class FlagsEnumValueToBoolMultiConverter : IMultiValueConverter
+{
+    uint targetValueCache;
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        //if (values is [uint targetValue, uint mask])
+        //{
+        targetValueCache = (uint)values[0];
+        uint mask = (uint)values[1];
+        return ((mask & targetValueCache) != 0);
+        //}
+        throw new NotImplementedException();
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        if (value is bool)
+        {
+            return [Enum.Parse(targetTypes[0], targetValueCache.ToString()), value];
+        }
+        throw new NotImplementedException();
+    }
+}
